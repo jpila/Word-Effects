fcc3e5093c03f6450ac3ff3cda3678bd79ce09b1
jpila <j.pilapil@hotmail.com>
2016-06-28 12:25:28 -0400

    Added more objects

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -24,26 +24,39 @@ int main(int argc, const char * argv[]) {
     int userOperation;
     NSLog(@"Select your operation: 1. Uppercase \n 2.Lowercase \n 3.Numerize \n 4.Canadianize \n 5.Respond \n 6.Despace it\n");
     scanf("%i", &userOperation);
-    
-    if (userOperation == 1){
-            printf("You have Chosen Uppercase\n");
-    }else if (userOperation==2){
-        printf("You have Chosen Lowercase\n");
-    }else if (userOperation==3){
-        printf("You have Chosen Numerize\n");
-    }else if (userOperation==4){
-        printf("You have Chosen Canadianize\n");
-    }else if (userOperation==5){
-        printf("You have Chosen Respond\n");
-    }else if (userOperation==6){
-        printf("You have Chosen Despace-it\n");
-    }
-    
+
     NSString *uppercaseString = [inputString uppercaseString];
     NSString *lowercaseString = [inputString lowercaseString];
     NSString *canadianString = [inputString stringByAppendingString: @" Eh?"];
     double  numberstring = [inputString doubleValue];
-                             
+    
+    if (userOperation == 1){
+        printf("You have Chosen Uppercase\n");
+        NSLog(@"%@", uppercaseString);
+    }else if (userOperation==2){
+        printf("You have Chosen Lowercase\n");
+        NSLog(@"%@", lowercaseString);
+    }else if (userOperation==3){
+        printf("You have Chosen Numerize\n");
+        NSLog(@"%f",numberstring);
+    }else if (userOperation==4){
+        printf("You have Chosen Canadianize\n");
+        NSLog(@"%@", canadianString);
+    }else if (userOperation==5){
+        NSString *lastchar = [inputString substringFromIndex:[inputString length] -2];
+        if ([lastchar isEqual: @"?\n"])
+            printf("I Dont Know");
+        else if ([lastchar isEqual: @"!\n"])
+            printf("Whoa! Calm Down!");
+        printf("You have Chosen Respond\n");
+    }else if (userOperation==6){
+        printf("You have Chosen Despace-it\n");
+        NSString *nospace = [inputString stringByReplacingOccurrencesOfString:@" " withString:@"-"];
+        NSLog(@"%@",nospace);
+    }
+    
+    
+    
     
     
     return 0;
