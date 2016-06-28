d6b48c0eca8fb666449afd50c9900c56e95883e2
jpila <j.pilapil@hotmail.com>
2016-06-27 20:29:33 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by JOSE PILAPIL on 2016-06-27.
+//  Copyright © 2016 JOSE PILAPIL. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
