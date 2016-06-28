66f9eba5854657af59b146fe276d0458bb1c61e6
jpila <j.pilapil@hotmail.com>
2016-06-27 20:51:17 -0400

    Prompts

--- Word Effects/main.m
+++ Word Effects/main.m
@@ -9,9 +9,13 @@
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-    }
+  
+    NSString *userInput;
+    int userChoice;
+    
+    NSLog(@"Select your operation: 1. Uppercase \n 2.Lowercase \n 3.Numerize \n 4.Canadianize \n 5.Respond \n 6.Despace it\n");
+    NSLog(@"Please input a string");
+    
+    
     return 0;
 }
